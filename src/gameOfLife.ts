2c9d756eaf9462e47ee6dbb49b73472d3de80ad2
Nicolas Poirier <dev.nicolaspoirier@gmail.com>
2024-07-05 14:41:56 +0200

    Implement: stay alive rule

--- src/gameOfLife.ts
+++ src/gameOfLife.ts
@@ -4,5 +4,19 @@ export enum State {
 }
 
 export function computeNextGeneration(grid: State[][]): State[][] {
-  return [[State.DEAD, State.DEAD, State.DEAD], [State.DEAD, State.DEAD, State.DEAD], [State.DEAD, State.DEAD, State.DEAD]]
+  let liveNeighboursCount = 0
+
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[row].length; col++) {
+      if (grid[row][col] === State.ALIVE && (row != 1 || col != 1)) {
+        liveNeighboursCount++
+      }
+    }
+  }
+
+  if (liveNeighboursCount < 2 || liveNeighboursCount > 3) {
+    return [[State.DEAD, State.DEAD, State.DEAD], [State.DEAD, State.DEAD, State.DEAD], [State.DEAD, State.DEAD, State.DEAD]]
+  } else {
+    return [[State.DEAD, State.DEAD, State.DEAD], [State.DEAD, State.ALIVE, State.DEAD], [State.DEAD, State.DEAD, State.DEAD]]
+  }
 }

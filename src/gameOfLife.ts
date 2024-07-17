bc02e185de7097cdac3f204596a30c5738c19495
Nicolas Poirier <dev.nicolaspoirier@gmail.com>
2024-07-17 15:11:14 +0200

    Refactor: use literals for possible neighbours shifts (no more magic numbers)

--- src/gameOfLife.ts
+++ src/gameOfLife.ts
@@ -9,22 +9,31 @@ const GENERATIVE_LIVE_NEIGHBOURS_COUNT = 3
 const OVERCROWDED_ABOVE_LIVE_NEIGHBOURS_COUNT = 3
 const UNDERPOPULATED_BELOW_LIVE_NEIGHBOURS_COUNT = 2
 
+const TOP_LEFT_NEIGHBOUR_SHIFT = { row: -1, col: -1 }
+const TOP_CENTER_NEIGHBOUR_SHIFT = { row: -1, col: 0 }
+const TOP_RIGHT_NEIGHBOUR_SHIFT = { row: -1, col: 1 }
+const CENTER_LEFT_NEIGHBOUR_SHIFT = { row: 0, col: -1 }
+const CENTER_RIGHT_NEIGHBOUR_SHIFT = { row: 0, col: 1 }
+const BOTTOM_LEFT_NEIGHBOUR_SHIFT = { row: 1, col: -1 }
+const BOTTOM_CENTER_NEIGHBOUR_SHIFT = { row: 1, col: 0 }
+const BOTTOM_RIGHT_NEIGHBOUR_SHIFT = { row: 1, col: 1 }
+
+const possibleNeighbourShifts = [
+  TOP_LEFT_NEIGHBOUR_SHIFT,
+  TOP_CENTER_NEIGHBOUR_SHIFT,
+  TOP_RIGHT_NEIGHBOUR_SHIFT,
+  CENTER_LEFT_NEIGHBOUR_SHIFT,
+  CENTER_RIGHT_NEIGHBOUR_SHIFT,
+  BOTTOM_LEFT_NEIGHBOUR_SHIFT,
+  BOTTOM_CENTER_NEIGHBOUR_SHIFT,
+  BOTTOM_RIGHT_NEIGHBOUR_SHIFT
+]
+
 function isCellInGrid(row: number, col: number, grid: Grid): boolean {
   return row >= 0 && row < grid.length && col >= 0 && col < grid[row].length
 }
 
 function getCellNeighbours(grid: Grid, cellRow: number, cellCol: number): State[] {
-  const possibleNeighbourShifts = [
-    { row: -1, col: -1 },
-    { row: -1, col: 0 },
-    { row: -1, col: 1 },
-    { row: 0, col: -1 },
-    { row: 0, col: 1 },
-    { row: 1, col: -1 },
-    { row: 1, col: 0 },
-    { row: 1, col: 1 }
-  ]
-
   return possibleNeighbourShifts
     .map(({ row, col }) => ({ row: cellRow + row, col: cellCol + col }))
     .filter(({ row, col }) => isCellInGrid(row, col, grid))
